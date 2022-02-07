23a78a75a8516ed184f1961e0141874a590c74ac
Jlucas93 <jlnascimentoandrade@gmail.com>
2022-02-07 19:01:56 -0300

    init

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 
-function fibonacci(num){
-
-    const sequencia =[0,1];
-    let a,b;
-
-    for( var i = 2; i <num; i++){
-        a = sequencia[i - 1];
-        b = sequencia [i -2];
-
-        sequencia.push(a+b);
+function nonFibonacci(num)
+{
+    // Dessa forma pulamos o 0 e 1 incial
+    let prepevious=1, previous=2, current=3;
+ 
+    // Garantindo que o número não seja negativo
+    while (n > 0)
+    {
+        // Criando a sequência Fibonacci
+        prepevious = previous;
+        previous = current;
+        current = prepevious + previous;
+ 
+        // (current - previous - 1) contagem de números não Fibonacci entre o atual e o anterior.
+        num = num - (current - previous - 1); 
     }
+ 
+    // Transformadno n em positivo
+    num = num + (current - previous - 1);
+ 
 
-    
-    console.log(sequencia)
+    // Somar o n positivo ao número de Fibonacci anterior para encontrar o n'ésimo não-fibonacci.
+    return previous + num;
 }
-
