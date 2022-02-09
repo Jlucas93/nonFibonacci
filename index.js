88c09b8d0460702cc77478dc63c64e3b8776142e
Jlucas93 <jlnascimentoandrade@gmail.com>
2022-02-09 20:16:42 -0300

    update

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 
-function nonFibonacci(num)
-{
-    if(num === 0){
+function nonFibonacci(num) {
+    if (num === 0) {
         console.error("Erro número não pode ser igual 0")
         return
-    }   
+    }
 
-    let prepevious=1, previous=2, current=3;
- 
+    let prepevious = 1, previous = 2, current = 3;
+
     // Garantindo que o número não seja negativo
-    while (num > 0)
-    {
+    while (num > 0) {
         // Criando a sequência Fibonacci
         prepevious = previous;
         previous = current;
         current = prepevious + previous;
- 
+
         // (current - previous - 1) contagem de números não Fibonacci entre o atual e o anterior.
-        num = num - (current - previous - 1); 
-        console.log("Atual: "+ current + " Valor de num: " + num)
-    } 
- 
+        num = num - (current - previous - 1);
+        console.log("Atual: " + current + " Valor de num: " + num)
+    }
+
     // Transformadno n em positivo
-    num = num + (current - previous - 1); 
- 
+    num = num + (current - previous - 1);
+
 
     // Somar o n positivo ao número de Fibonacci anterior para encontrar o n'ésimo não-fibonacci.
     return previous + num;
